6135b2a2cba477679a9f7d0b4b69e05d812d74a8
JohnnyMoonlight <41798271+JohnnyMoonlight@users.noreply.github.com>
2020-06-15 12:47:33 +0200

    Update RowEditor.js

--- js/RowEditor.js
+++ js/RowEditor.js
@@ -8,15 +8,28 @@ class RowEditor {
     this.originalRowValues = null;
     }
 
-    editRow(indexOfCallingRow) {
+    deleteRow(indexOfCallingRow) {
         if (this.rowInEditMode) {
             this.resetRow();
         }
-        var listener = document.addEventListener('keyup', (e) => this.escapeHandler(e));
+        table.row(indexOfCallingRow).remove().draw();
+    }
+
+    editRow(indexOfCallingRow) {
+        let indexOfCallingRowSanitized = indexOfCallingRow+1;
+        //if command is called on the same row, save the values
+        if (this.rowInEditMode===indexOfCallingRowSanitized) {
+            this.saveValues();
+            return;
+        }
+        //if command is called on a different row, reset
+        if (this.rowInEditMode) {
+            this.resetRow();
+        }
         let c = this.editRowSettings;
-        this.setRowEditMode(indexOfCallingRow);
-        this.setOriginalRowValuesFor(indexOfCallingRow);
-        let row = $(this.table).find('tr').eq(indexOfCallingRow);
+        this.setRowEditMode(indexOfCallingRowSanitized);
+        this.setOriginalRowValuesFor(indexOfCallingRowSanitized);
+        let row = $(this.table).find('tr').eq(indexOfCallingRowSanitized);
         row.find('td').each((index, element)=>{
             if(index in c) {
                 $(element).html(this.getInputHTML(element, index, c));
@@ -24,26 +37,12 @@ class RowEditor {
         });
     }
 
-    escapeHandler (event) {
-        if (event.key ==="Escape") {
-            this.resetRow();
-            this.setRowEditMode(null);
-            this.setOriginalRowValuesFor(null);
-        }
-        if (event.key==="Enter") {
-            this.setOutput(this.rowInEditMode);
-            this.setRowEditMode(null);
-            this.setOriginalRowValuesFor(null);
-        }
-    }
-
     setRowEditMode (index) {
         this.rowInEditMode = index;
     }
 
     setOriginalRowValuesFor (index) {
-        let row = [];
-        this.originalRowValues=$(this.table).find('tr').eq(index).html();
+        this.originalRowValues=this.dataTable.row(index-1);
     }
 
     setOutput(index) {
@@ -59,9 +58,30 @@ class RowEditor {
         });
     }
 
+    /*save value needs to
+     - firstly, check if any row is being edited and could have new input,
+     - secondly, set the output view (method setOutput()) and
+     - thirdly, clear the edit modals (methods rowEditMode() and setOriginalRowValues())
+    */
+    saveValues () {
+    if (this.rowInEditMode) {
+        this.setOutput(this.rowInEditMode);
+        this.setRowEditMode(null);
+        this.setOriginalRowValues=null;
+        $("[id*='tooltip']" ).remove();
+    }
+    }
+
+    resetRow (index) {
+        $(this.table).find('tr').eq(index).html(this.originalRowValues);
+    }
+
     resetRow () {
         if (this.rowInEditMode) {
-            $(this.table).find('tr').eq(this.rowInEditMode).html(this.originalRowValues);
+            this.dataTable.row(this.rowInEditMode-1).data(this.originalRowValues.data());
+            this.dataTable.draw(false);
+            this.setRowEditMode(null);
+            this.setOriginalRowValuesFor(null);
         }
     }
 
@@ -83,7 +103,7 @@ class RowEditor {
                 let html = '<select id=\''+ this.classIdOf(index) +'\'>';
                 let options = settings[index]["options"];
                 for (let i = 1; i<=Object.keys(options).length; i++){
-                    html+="<option value=\""+options[i].value+"\">"+options[i].title+"</option>"
+                    html+="<option value="+options[i].value+">"+options[i].title+"</option>"
                 }
                 html+="</select>"
                 return html;
